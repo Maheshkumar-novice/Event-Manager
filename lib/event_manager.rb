872b77c0ae071f27b5fb4805a9aa09823bb9f9ee
Maheshkumar <callmeelprofessor@gmail.com>
2021-08-21 11:48:44 +0530

    Feature: Add Clean Phone Number

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -9,15 +9,25 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone)
+  phone = phone.delete('^0-9')
+  if phone.length == 11 && phone[0] == '1' || phone.length == 10
+    phone = phone[-10..-1]
+    [phone[0..2], phone[3..5], phone[6..-1]].join('-')
+  elsif phone.length != 10
+    'Valid Number Not Provided!'
+  end
+end
+
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
   begin
-    legislators = civic_info.representative_info_by_address(address: zipcode,
-                                                            levels: 'country',
-                                                            roles: %w[
-                                                              legislatorUpperBody legislatorLowerBody
-                                                            ]).officials
+    civic_info.representative_info_by_address(address: zipcode,
+                                              levels: 'country',
+                                              roles: %w[
+                                                legislatorUpperBody legislatorLowerBody
+                                              ]).officials
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
@@ -49,9 +59,10 @@ contents.each do |row|
   puts "Creating thanks_#{id}.html..."
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  phone = clean_phone_number(row[:homephone])
+  puts phone + '=>' + row[:homephone]
   legislators = legislators_by_zipcode(zipcode)
+  # form_letter = erb_template.result(binding)
 
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id, form_letter)
+  # save_thank_you_letter(id, form_letter)
 end
