9d5082432269415dfc4180f08021f3aad6c312ad
Maheshkumar <callmeelprofessor@gmail.com>
2021-08-24 21:35:36 +0530

    Bug fixes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -23,11 +23,11 @@ def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
   begin
-    civic_info.representative_info_by_address(address: zipcode,
-                                              levels: 'country',
-                                              roles: %w[
-                                                legislatorUpperBody legislatorLowerBody
-                                              ]).officials
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
@@ -60,7 +60,7 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phone = clean_phone_number(row[:homephone])
-  
+
   legislators = legislators_by_zipcode(zipcode)
   form_letter = erb_template.result(binding)
 
